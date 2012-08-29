833fdc98959808fdbca90ebb85baa86024133a48
Stephen Eley <sfeley@gmail.com>
2012-08-29 10:38:26 -0400

    Removed 'live mode' from specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@
 
 require "bundler/setup"
 require "em-aws"
-require "webmock/rspec" unless ENV['AWS_TEST_MODE'] == 'live'
+require "webmock/rspec"
 
 Dir[File.join File.dirname(__FILE__), 'support', '**', '*.rb'].each {|f| require f}
 
@@ -18,25 +18,11 @@ RSpec.configure do |config|
 
   # EM::AWS.loglevel = ::Logger::DEBUG
 
-  # Run in 'mock' or 'live' mode based on the AWS_TEST_MODE variable. (Defaults to 'mock.')
-  # If in live mode, the credentials must also be set in the environment.
-  if ENV['AWS_TEST_MODE'] == 'live'
-    config.filter_run_excluding mock: true
-    config.before(:all) do
-      EventMachine::AWS.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
-      EventMachine::AWS.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
-    end
-  else
-    config.filter_run_excluding live: true
-    WebMock.disable_net_connect!
-    config.before(:each) do
-      EventMachine::AWS.aws_access_key_id = 'FAKE_KEY'
-      EventMachine::AWS.aws_secret_access_key = 'FAKE_SECRET'
-    end
-    config.before(:each, mock: true) do
-      raise "Enable loading from the YAML!"
-    end
+  WebMock.disable_net_connect!
+  config.before(:each) do
+    EventMachine::AWS.aws_access_key_id = 'FAKE_KEY'
+    EventMachine::AWS.aws_secret_access_key = 'FAKE_SECRET'
   end
-  
+
   config.include EventMachineHelper
 end
